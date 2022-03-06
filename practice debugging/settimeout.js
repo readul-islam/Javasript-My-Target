224abd7eec39010b2f4df261596b8a96ac9953b0
akmahamud <am3153636@gmail.com>
2022-03-06 09:56:02 +0600

    settime out recape

---
+++ practice debugging/settimeout.js
@@ -0,0 +1,9 @@
+const practice = () =>{
+    console.log('this is one task')
+    setTimeout( () => {
+        console.log('this is settime out function and task two')
+       
+    },3000);
+    console.log('this is three task');
+}
+ practice();
